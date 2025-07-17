32a6fb154e7bd3b1eab0f511f5160c8fd590e598
Benjamín Quiroz Rojas <benjamin.quiroz.rojas@gmail.com>
2025-07-17 12:36:17 -0400

    Creacion proyecto

---
+++ reservas-backend/src/test/java/com/quirozaurio/api/ReservasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quirozaurio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
